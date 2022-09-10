e64b53fb7f1320b62346537f7b6dd5b08b30888a
GarfieldCK <chonlasit.kkaew@gmail.com>
2022-09-10 17:43:02 +0700

    feat:containerize the application

--- chat.py
+++ chat.py
@@ -12,31 +12,6 @@ from utils.dialogue_manager import DialogueManager
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-
-# def get_response(msg):
-#     sentence = tokenize(msg)
-#     X = bag_of_words(sentence, all_words)
-#     X = X.reshape(1, X.shape[0])
-#     X = torch.from_numpy(X).to(device)
-#
-#     output = model(X)
-#     _, predicted = torch.max(output, dim=1)
-#
-#
-#     print("Check here {}".format(predicted.item()))
-#     tag = tags[predicted.item()]
-#
-#
-#     #TODO: Add this part for pattern matching
-#     probs = torch.softmax(output, dim=1)
-#     prob = probs[0][predicted.item()]
-#     if prob.item() > 0.75:
-#         for intent in intents['intents']:
-#             if tag == intent["tag"]:
-#                 return random.choice(intent['responses'])
-#
-#     return "I do not understand..."
-
 def _get_response(msg: str, msg_manager):
 
     _response = msg_manager.generate_answer(msg)
