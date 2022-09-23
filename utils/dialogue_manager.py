ca951c093abc14f570b58730fc6e1825e8140249
GarfieldCK <chonlasit.kkaew@gmail.com>
2022-09-23 17:02:14 +0700

    feature:add intent classification and sentence similarity

--- utils/dialogue_manager.py
+++ utils/dialogue_manager.py
@@ -5,25 +5,26 @@ import pandas as pd
 from pythainlp import word_tokenize
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
-from pythainlp_utils import thai_bag_of_words, thai_tokenize
+from utils.pythainlp_utils import thai_bag_of_words, thai_tokenize
 
 from utils.helper import _float_converter
 
 class DialogueManager():
 
-    def __init__(self, answer_model, intent_model,input_size, hidden_size, output_size, all_words, tags, device):
+    def __init__(self,data_corpus, answer_model, intent_model,input_size, hidden_size, output_size, all_words, tags, device):
         """ dataset cols -> [Intents,Keys, Keys_vector,Values]
         """
         # Model && corpus initiate
         self.model = answer_model
         self.intent_tagging = intent_model
-        self.dataset = pd.read_csv("../Projects/configs/data_corpus_v2.csv")
+        self.dataset = data_corpus
 
         # Corpus parameter declarations
         self.QUESTION = self.dataset.Keys
         self.QUESTION_VECTORS = self.dataset.Keys_vector
         self.ANSWER = self.dataset.Values
         self.COSINE_THRESHOLD = 0.5
+        self.CONF_SCORE = 0.65
 
         # Model parameters declaration
         self.input_size = input_size
@@ -37,7 +38,8 @@ class DialogueManager():
         """ Receive a "sentence" and encode to vector in dimension 300
             Step : 
             1.) Word tokenize from "sentence"
-            2.) Create a vector size == dimension
+            2.) C
+    model =  SentenceTransformer('checkpoints/simcse-model-thai-version-supAIkeyword')reate a vector size == dimension
             3.) Add up the vector from the dictionary of index2word
             4.) return sentence vectorize
         """
@@ -65,15 +67,6 @@ class DialogueManager():
 
         return self.model.encode([sentenced])
 
-    def tagging_prob(self, model_output, predicted):
-        """ Calculate a probability of model output
-        """
-        probs = torch.softmax(model_output, dim=1)
-        prob = probs[0][predicted.item()]
-
-        return prob.item()
-
-
     def tagging(self, sentenced : str):
         """ Receive a user input and predict the tags 
         Parameters
@@ -84,6 +77,7 @@ class DialogueManager():
                 tag : string
                     : Refer to which class a query vector is, Reference from tag in configs/intents.json
         """
+        tag_dict ={}
 
         sentenced = thai_tokenize(sentenced)
         X = thai_bag_of_words(sentenced, self.all_words)
@@ -92,11 +86,18 @@ class DialogueManager():
 
         output = self.intent_tagging(X)
         _, predicted = torch.max(output, dim=1)
-        tag = self.tags[predicted.item()]
+        probs = torch.softmax(output, dim=1)
+        prob = probs[0][predicted.item()]
 
-        prob = self.tagging_prob(output, predicted)
+        tag_dict.update({self.tags[predicted.item()] : prob.item()})
 
-        return tag
+
+        # Generate tagging where prob > CONF_SCORE
+        # for idx, p in enumerate(probs[0]):
+        #     if p > self.CONF_SCORE:
+        #         tag_dict.update({self.tags[idx] : p.item()})
+
+        return tag_dict
 
     def semantic_search(self, query_text):
         """ Search the matching question from the corpus, grasp the "keys" from the probability that passing criterion.
@@ -110,38 +111,63 @@ class DialogueManager():
         
         query_vec = self.sent_embeddings(query_text)
         
-        sim_score = []
         most_relavance_dict= {}
         #Step 1 : Generate a key from tagging
-        _intent_ls = list(set(self.dataset.Intents.tolist()))
+        tag_dict = self.tagging(query_text)
         
-        for idx in range(len(_intent_ls)):
-            answer_keys = self.dataset.loc[self.dataset.Intents == _intent_ls[idx]].Keys_vector.tolist() 
-            for answer_key in answer_keys:
-                answer_vec = _float_converter(answer_key)
-                sim = cosine_similarity(query_vec, answer_vec)
+        #Step 2 : Pick the key vector from each intent and measure the similarit 
+        t = list(tag_dict)[0]
+        answer_keys = self.dataset.loc[self.dataset.Intents == t].Keys_vector.tolist()
+        for a_key in answer_keys:
+            answer_vec = _float_converter(a_key)
+            sim = cosine_similarity(query_vec, answer_vec)
+            voting_prob = self.voting(tag_dict[t], sim)
 
-                #TODO : Add active learning loop at this point(optional)
-                if sim > self.COSINE_THRESHOLD:
-                    most_relavance_dict.update({_intent_ls[idx] : self.dataset.loc[self.dataset.Intents == _intent_ls[idx]].Values.tolist()[0]})
-                    sim_score.append(sim)
-                    break
-                else:
-                    pass
+            # 3.) If score > threshold, pick a values from "Values" columns and update dictionary as "intent" : "Values"
+            if voting_prob > self.COSINE_THRESHOLD:
+                most_relavance_dict.update({t : self.dataset.loc[self.dataset.Intents == t].Values.tolist()[0]})
+                break
+            else:
+                pass
 
 
-        return most_relavance_dict, sim_score
+        # for t in tag_dict.keys():
+        #     answer_keys = self.dataset.loc[self.dataset.Intents == t].Keys_vector.tolist() 
+        #     for answer_key in answer_keys:
+        #         answer_vec = _float_converter(answer_key)
+        #         sim = cosine_similarity(query_vec, answer_vec)
+
+        #         # Calculate a probability of two event
+        #         voting_prob = self.voting(tag_dict[t], sim)
+
+        #         #TODO : Add active learning loop at this point(optional)
+        #         
+        #         if voting_prob > self.COSINE_THRESHOLD:
+        #             most_relavance_dict.update({t : self.dataset.loc[self.dataset.Intents == t].Values.tolist()[0]})
+        #             sim_score.append(voting_prob)
+        #             break
+        #         else:
+        #             pass
+
+
+        return most_relavance_dict
+
+    def voting(self, tag_prob : float, values_prob : float):
+        """ Weighting the answer from "intent classification" and "Pattern matching"
+        """
+
+        return (tag_prob + values_prob) / 2
+
 
     def generate_answer(self, question):
         """ Query the matching "question" and return "answer"
         """
-        answer_dict, sim_score = self.semantic_search(question)
+        answer_dict = self.semantic_search(question)
 
         if len(answer_dict) != 0:
             answer = ''
-            for idx, values in enumerate(answer_dict.values()): 
+            for values in (answer_dict.values()): 
                                 
-                print("Probability : {}".format(sim_score[idx]))
                 answer += "* " + values + "\n"
                 
         else:
@@ -152,22 +178,4 @@ class DialogueManager():
             _f.close()
 
         return answer
-
-if __name__ == "__main__" :
-
-    # For debug :
-    q_vec = "ต้องเสียค่าใช้จ่ายในการสมัครมั้ยครับ"
-    model =  SentenceTransformer('checkpoints/simcse-model-thai-version-supAIkeyword')
-    msg_manager = DialogueManager(model)
-
-    resp = msg_manager.generate_answer(q_vec)
-    print(resp)
-
-
-
-
-
-
-    
-
 
