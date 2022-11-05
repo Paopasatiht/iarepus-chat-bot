fd42de7291ddac38b25835010ad3faf1c5855da1
GarfieldCK <chonlasit.kkaew@gmail.com>
2022-11-05 22:01:13 +0700

    enhancement: hot fix app.py

--- app.py
+++ app.py
@@ -28,17 +28,22 @@ CORS(app)
 config_file = "/Projects/configs/config.yaml"
 cfg = YamlParser(config_file)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Load data
 data_corpus = pd.read_csv(cfg["DATA_CORPUS"]["data_csv"])
+keyword_csv = pd.read_csv(cfg["DATA_CORPUS"]["keyword_csv"])
+print("Prepare data corpus")
 
 # Load sentence embedded model
 answer_model = SentenceTransformer(cfg["MODEL"]["answer_model"])
+print("Load sentence embedded model . . .")
 
 # Load intent classification model
 intent_path = cfg["MODEL"]["intent_model"]
 
 # Load model from weight files
 intent_model = pickle.load(open(intent_path, 'rb'))
-
+print("Load intent model . . .")
 
 # Declare count vectorizer
 tf_vectorizer = CountVectorizer()
@@ -47,9 +52,20 @@ vectors = tf_vectorizer.fit_transform(data_corpus.Keys)
 # Load sentence embedded model
 answer_model = SentenceTransformer(cfg["MODEL"]["answer_model"])
 wv_model = KeyedVectors.load_word2vec_format('/Projects/checkpoints/LTW2V_v0.1.bin', binary=True, unicode_errors='ignore')
+print("Load word vector model . . .")
 
  # tags declaration
 kw = list(cfg["KEYWORD_INTENT"].keys())
+
+# Declared a dictionary from config.yaml files
+config_dict = cfg["KEYWORD_INTENT"]
+
+# Declare a custom dictionary :
+print("Prepare custom dictionary ...")
+custom_ls = cfg["CUSTOM_DICT"]["words"]
+_dict = {k for k in custom_ls}
+custom_words = _dict.union(thai_words())
+custom_dictionary_trie = Trie(custom_words)
 
 # Main function here :
 @app.get("/")
@@ -60,9 +76,11 @@ def index_get():
 @app.post("/predict")
 def predict():
     text = request.get_json().get("message")
-    msg_manager = DialogueManager(data_corpus, wv_model, answer_model, intent_model, tf_vectorizer, device, kw)
+    msg_manager = DialogueManager(data_corpus, wv_model, answer_model, intent_model, tf_vectorizer, config_dict, custom_dictionary_trie, keyword_csv)
     response = _get_response(text, msg_manager)
     message =  {"answer" : response}
+
+
     return jsonify(message)
 
 # Runing the app :
